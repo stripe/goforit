8899a788764b19414032291cbc6f5632e80a9ce0
Aditya Mukerjee <dev@chimeracoder.net>
2017-03-06 12:20:57 -0500

    Add Example function

---
+++ examples_test.go
@@ -0,0 +1,22 @@
+package goforit
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func Example(t *testing.T) {
+	// flags.csv contains comma-separated flag names and sample rates.
+	// See: fixtures/flags_example.csv
+	backend := BackendFromFile("flags.csv")
+	Init(30*time.Second, backend)
+
+	if Enabled("go.sun.mercury") {
+		fmt.Println("The go.sun.mercury feature is enabled for 100% of requests")
+	}
+
+	if Enabled("go.stars.money") {
+		fmt.Println("The go.stars.money feature is enabled for 50% of requests")
+	}
+}
